f6f59d65102e5def90cdbf5f7ddfc6470dca2fe2
Jean-Francois Couture <jfcouture@gmail.com>
2010-07-06 12:35:50 -0400

    import generators template most likely to be modified

---
+++ lib/templates/mongoid/model/model.rb
@@ -0,0 +1,41 @@
+class <%= class_name %><%= " < #{options[:parent].classify}" if options[:parent] %>
+  
+  ### Includes
+<% unless options[:parent] -%>
+  include Mongoid::Document
+<% end -%>
+<% if options[:timestamps] -%>
+  include Mongoid::Timestamps
+<% end -%>
+<%= 'include Mongoid::Versioning' if options[:versioning] -%>
+  
+  ### Plugins magic
+
+  
+  ### Fields declaration 
+<% attributes.reject{|attr| attr.reference?}.each do |attribute| -%>
+  field :<%= attribute.name %>, :type => <%= attribute.type_class %>
+<% end -%>
+  
+  
+  ### Relationships
+<% attributes.select{|attr| attr.reference? }.each do |attribute| -%>
+  embedded_in :<%= attribute.name%>, :inverse_of => :<%= class_name.tableize %>
+<% end -%>
+  
+  
+  ### Validations
+  
+  
+  ### Callbacks
+  
+  
+  ### Scopes
+  
+  
+  ###########
+  # Other
+  
+  
+  
+end
